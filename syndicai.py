14112383282dfb169c6bc1e700ca993b491647d0
Akash Panchal <akashp1712@gmail.com>
2022-06-23 22:33:13 +0530

    Update syndicai.py

--- syndicai.py
+++ syndicai.py
@@ -1,32 +1,24 @@
 import json
-import torch
-import json 
-from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
-
+import spacy
 
 class PythonPredictor:
     def __init__(self, config):
-        self.model = T5ForConditionalGeneration.from_pretrained('t5-small')
-        self.tokenizer = T5Tokenizer.from_pretrained('t5-small')
-        self.device = torch.device('cpu')
+        # load spacy model
+        nlp = spacy.load('en_core_web_sm')
       
     def predict(self, payload):
         print(payload)
         data = json.loads(payload)
         payload_text = data["text"]
         preprocessed_text = payload_text.strip().replace("\n","")
-        t5_prepared_Text = "summarize: " + preprocessed_text
+        
+        # load data
+        sentence = "Apple is looking at buying U.K. startup for $1 billion"
+        doc = nlp(preprocessed_text)
 
-        tokenized_text = self.tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(self.device)
+        # print entities
+        for ent in doc.ents:
+            print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
-        # summmarize 
-        summary_ids = self.model.generate(tokenized_text,
-                                            num_beams=4,
-                                            no_repeat_ngram_size=2,
-                                            min_length=30,
-                                            max_length=100,
-                                            early_stopping=True)
-
-        summary = self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)    
-        return {'original': payload_text, 'summary': summary}
+        return {'original': payload_text, 'ner': docs.ents}
       
